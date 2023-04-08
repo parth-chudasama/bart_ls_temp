4295ac3707de80b8f322d619379b6c986d151152
parth-chudasama <69851359+parth-chudasama@users.noreply.github.com>
2023-04-08 17:55:44 +0530

    Update fb_audio_pretraining.py

--- fairseq-py/fairseq/tasks/fb_audio_pretraining.py
+++ fairseq-py/fairseq/tasks/fb_audio_pretraining.py
@@ -1,93 +1,93 @@
-# Copyright (c) 2017-present, Facebook, Inc.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the LICENSE file in
-# the root directory of this source tree. An additional grant of patent rights
-# can be found in the PATENTS file in the same directory.
+# # Copyright (c) 2017-present, Facebook, Inc.
+# # All rights reserved.
+# #
+# # This source code is licensed under the license found in the LICENSE file in
+# # the root directory of this source tree. An additional grant of patent rights
+# # can be found in the PATENTS file in the same directory.
 
-import logging
-import os
-from itertools import count
+# import logging
+# import os
+# from itertools import count
 
-from fairseq.data.audio.fb_everstore_audio_dataset import EverstoreAudioDataset
+# from fairseq.data.audio.fb_everstore_audio_dataset import EverstoreAudioDataset
 
-from . import LegacyFairseqTask, register_task
-
-
-logger = logging.getLogger(__name__)
+# from . import LegacyFairseqTask, register_task
 
 
-@register_task("fb_audio_pretraining")
-class SpeechPretrainingTask(LegacyFairseqTask):
-    @staticmethod
-    def add_args(parser):
-        """Add task-specific arguments to the parser."""
-        parser.add_argument("data", help="path to data directory")
-        parser.add_argument(
-            "--sample-rate",
-            default=16000,
-            type=int,
-            help="target sample rate. audio files will be up/down sampled to this rate",
-        )
-        parser.add_argument(
-            "--max-sample-size",
-            default=None,
-            type=int,
-            help="max sample size to crop to for batching. default = min sample length",
-        )
-        parser.add_argument(
-            "--min-sample-size",
-            default=None,
-            type=int,
-            help="min sample size to crop to for batching. default = same as --max-sample-size",
-        )
+# logger = logging.getLogger(__name__)
 
-    def __init__(self, args):
-        super().__init__(args)
 
-    @classmethod
-    def setup_task(cls, args, **kwargs):
-        """Setup the task (e.g., load dictionaries).
+# @register_task("fb_audio_pretraining")
+# class SpeechPretrainingTask(LegacyFairseqTask):
+#     @staticmethod
+#     def add_args(parser):
+#         """Add task-specific arguments to the parser."""
+#         parser.add_argument("data", help="path to data directory")
+#         parser.add_argument(
+#             "--sample-rate",
+#             default=16000,
+#             type=int,
+#             help="target sample rate. audio files will be up/down sampled to this rate",
+#         )
+#         parser.add_argument(
+#             "--max-sample-size",
+#             default=None,
+#             type=int,
+#             help="max sample size to crop to for batching. default = min sample length",
+#         )
+#         parser.add_argument(
+#             "--min-sample-size",
+#             default=None,
+#             type=int,
+#             help="min sample size to crop to for batching. default = same as --max-sample-size",
+#         )
 
-        Args:
-            args (argparse.Namespace): parsed command-line arguments
-        """
-        return cls(args)
+#     def __init__(self, args):
+#         super().__init__(args)
 
-    def create_dataset(self, manifest):
-        logger.info("Reading manifest " + manifest)
-        return EverstoreAudioDataset(
-            manifest,
-            sample_rate=self.args.sample_rate,
-            max_sample_size=self.args.max_sample_size,
-            min_sample_size=self.args.min_sample_size,
-            min_length=16000,
-        )
+#     @classmethod
+#     def setup_task(cls, args, **kwargs):
+#         """Setup the task (e.g., load dictionaries).
 
-    def load_dataset(self, split, epoch=1, **kwargs):
-        """Load a given dataset split.
+#         Args:
+#             args (argparse.Namespace): parsed command-line arguments
+#         """
+#         return cls(args)
 
-        Args:
-            split (str): name of the split (e.g., train, valid, test)
-        """
-        manifest = os.path.join(self.args.data, "{}.tsv".format(split))
-        if os.path.isfile(manifest):
-            self.datasets[split] = self.create_dataset(manifest)
-        else:
-            if not hasattr(self, "dataset_parts"):
-                self.dataset_parts = []
-                for i in count(1):
-                    manifest = os.path.join(self.args.data, "{}{}.tsv".format(split, i))
-                    if not os.path.isfile(manifest):
-                        break
-                    self.dataset_parts.append(manifest)
-                if len(self.dataset_parts) == 0:
-                    raise FileNotFoundError(manifest)
-            part_idx = (epoch - 1) % len(self.dataset_parts)
-            self.datasets[split] = self.create_dataset(self.dataset_parts[part_idx])
+#     def create_dataset(self, manifest):
+#         logger.info("Reading manifest " + manifest)
+#         return EverstoreAudioDataset(
+#             manifest,
+#             sample_rate=self.args.sample_rate,
+#             max_sample_size=self.args.max_sample_size,
+#             min_sample_size=self.args.min_sample_size,
+#             min_length=16000,
+#         )
 
-    @property
-    def target_dictionary(self):
-        """Return the :class:`~fairseq.data.Dictionary` for the language
-        model."""
-        return None
+#     def load_dataset(self, split, epoch=1, **kwargs):
+#         """Load a given dataset split.
+
+#         Args:
+#             split (str): name of the split (e.g., train, valid, test)
+#         """
+#         manifest = os.path.join(self.args.data, "{}.tsv".format(split))
+#         if os.path.isfile(manifest):
+#             self.datasets[split] = self.create_dataset(manifest)
+#         else:
+#             if not hasattr(self, "dataset_parts"):
+#                 self.dataset_parts = []
+#                 for i in count(1):
+#                     manifest = os.path.join(self.args.data, "{}{}.tsv".format(split, i))
+#                     if not os.path.isfile(manifest):
+#                         break
+#                     self.dataset_parts.append(manifest)
+#                 if len(self.dataset_parts) == 0:
+#                     raise FileNotFoundError(manifest)
+#             part_idx = (epoch - 1) % len(self.dataset_parts)
+#             self.datasets[split] = self.create_dataset(self.dataset_parts[part_idx])
+
+#     @property
+#     def target_dictionary(self):
+#         """Return the :class:`~fairseq.data.Dictionary` for the language
+#         model."""
+#         return None
